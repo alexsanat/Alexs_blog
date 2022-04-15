58223b2b78fa9d89937ac77d1aefd7f3e8b640c2
alex <alexander_tanas@mail.ru>
2022-04-15 08:51:42 +0100

    Complete the project

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-SECRET_KEY = os.environ.get('APP_SECRET_KEY')
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URL'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
